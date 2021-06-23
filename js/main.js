59b409f72a34fb373e1db7dcf22122a57b245996
Leonel <leoneloliver@gmail.com>
2021-06-23 18:08:33 -0400

    testing ga tags

--- js/main.js
+++ js/main.js
@@ -289,3 +289,14 @@
 	});		
 
 })(jQuery);
+
+
+
+var getOutboundLink = function(url) {
+	gtag('event', 'click', {
+	  'event_category': 'outbound',
+	  'event_label': url,
+	  'transport_type': 'beacon',
+	  'event_callback': function(){document.location = url;}
+	});
+  }
