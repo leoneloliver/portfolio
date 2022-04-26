b0d19a778c6bdcfe8075059c88cc1fcfa4a2b319
Leonel Oliveira <leoneloliveira@VOL-VHQ-ML7261.local>
2022-04-26 15:47:23 -0400

    New version set font

--- js/mysetfont_v3.js
+++ js/mysetfont_v3.js
@@ -18,16 +18,16 @@ if(hasSetfont){
         let styleSheet = `
         .container-font{
             position: fixed;
-            width: 138px;
+            width: 160px;
             top: 0;
             right: 0;
             z-index: 999999999999;
             border: 1px solid #000;
             background: #000;
             padding: 12px;
+            text-align: center;
         }
         .container-font button{
-            font-size: 10px;
             padding: 0;
             margin: 0;
             height: 31px;
@@ -38,6 +38,9 @@ if(hasSetfont){
             border-radius: 40px;
             z-index: 999999999;
             position: relative;
+            font-size: 9px;
+            font-weight: 600;
+            letter-spacing: 2px;
         }
         .inner-result{
             background-color: #323232;
@@ -45,7 +48,7 @@ if(hasSetfont){
             padding: 12px;
             font-size: 12px;
             border-top: 3px solid #000;
-            position: absolute;
+            position: fixed;
             bottom: 0;
             width: 100%;
             z-index: 999999999;
@@ -65,12 +68,12 @@ if(hasSetfont){
             cursor: pointer;	
         }
         #f_close {
-            position: absolute;
+            position: fixed;
             z-index: 9999999999;
             color: #403737;
             width: 25px;
-            bottom: 81px;
-            right: 0;
+            bottom: 265px;
+            right: 12px;
             padding: 0;
             margin: 0;
             line-height: 10px;
@@ -78,9 +81,38 @@ if(hasSetfont){
             background: #cccccc;
             border-radius: 40px;
             padding-left: 3px;
+            font-size: 12px !important;
         }
         .f_hide{
             display:none;
+        }
+        textarea#text-result {
+            color: #0ed88c;
+            width: 100%;
+            border: 0;
+            padding: 12px;
+            font-size: 12px !important;
+            line-height: 22px;
+        }
+        button#f_copy {
+            position: fixed;
+            z-index: 9999999999999;
+            right: 18px;
+            bottom: 58px;
+            /* width: 12px; */
+            padding: 0;
+            margin: 0;
+            line-height: 12px;
+            height: 30px;
+            font-family: verdana;
+            font-size: 12px !important;
+            font-weight: 600;
+            background: #399f84;
+            color: #fff;
+        }
+        button#f_copy:hover{
+            background: #efefef;
+            color: #000;
         }
     `;
         let css = document.createElement('style');
@@ -98,7 +130,7 @@ if(hasSetfont){
         overlayDiv.innerHTML = `<div id="font-result"></div>
         <div class="container-font f_hide">
             <input type="number" id="fontcontrol" id="fontcontrol" min="0" max="100">
-            <button id="copyfont">Copy</button>
+            <button id="copyfont">Check Fonts</button>
         </div>`;
         document.body.insertBefore(overlayDiv, document.body.firstChild);
 
@@ -125,12 +157,27 @@ if(hasSetfont){
         function getAllFonts(){
             let fontfinal = "";
             for(let i = 0; i < elFont.length; i++){
-                fontfinal += "> "+elFont[i].innerText.substring(0, 20) +' -> '+ window.getComputedStyle(elFont[i]).fontSize +' <br> ';
+                fontfinal += "> "+elFont[i].innerText.substring(0, 20) +' -> '+ window.getComputedStyle(elFont[i]).fontSize +' \r\n';
             }
-            document.querySelector('#font-result').innerHTML = "<div class='inner-result'>"+fontfinal+"</div><button id='f_close'>X</button>";
+            document.querySelector('#font-result').innerHTML = "<div class='inner-result'><textarea id='text-result' name='text-result' rows='4' cols='50'>"+fontfinal+"</textarea></div><button id='f_close'>X</button><button id='f_copy'>&nbsp;copy [] </button>";
 
             document.querySelector('#f_close').addEventListener("click", function(){
                 document.querySelector('#font-result').innerHTML = "";
+            })
+
+            document.querySelector('#f_copy').addEventListener("click", function(){
+                var copyText = document.getElementById("text-result");
+
+                /* Select the textarea field */
+                copyText.select();
+                copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+                /* Copy the text inside the text field */
+                navigator.clipboard.writeText(copyText.value);
+
+                /* Alert the copied text */
+                // alert("Copied the text: " + copyText.value);
+                document.execCommand('copy');
             })
         }
 
