a97298f7fae324c0127d95a0a8f699e38b05c5d1
Leonel Oliveira <leoneloliveira@VOL-VHQ-ML7261.local>
2022-04-26 10:22:49 -0400

    Added set font from cdn

--- js/setfont.js
+++ js/setfont.js
@@ -11,7 +11,7 @@ if(hasSetfont){
 
     console.log('setFont is running!');
 
-    alert('set font is ON');
+    alert('Setfont tool was turned ON!');
 
     const addStyle = () => {
 
@@ -40,10 +40,15 @@ if(hasSetfont){
             position: relative;
         }
         .inner-result{
-            background-color: #000;
-            color: #07d88c;
+            background-color: #323232;
+            color: #ddffe2;
             padding: 12px;
             font-size: 12px;
+            border-top: 3px solid #000;
+            position: absolute;
+            bottom: 0;
+            width: 100%;
+            z-index: 999999999;
         }
         #fontcontrol{
             font-size: 20px;
@@ -59,6 +64,24 @@ if(hasSetfont){
         [data-font] {
             cursor: pointer;	
         }
+        #f_close {
+            position: absolute;
+            z-index: 9999999999;
+            color: #403737;
+            width: 25px;
+            bottom: 81px;
+            right: 0;
+            padding: 0;
+            margin: 0;
+            line-height: 10px;
+            height: 25px;
+            background: #cccccc;
+            border-radius: 40px;
+            padding-left: 3px;
+        }
+        .f_hide{
+            display:none;
+        }
     `;
         let css = document.createElement('style');
         css.type = "text/css";
@@ -72,8 +95,8 @@ if(hasSetfont){
 
         let overlayDiv = document.createElement('DIV');
         overlayDiv.id = "setFont";
-        overlayDiv.innerHTML = `<div id="font-result"></div>
-        <div class="container-font">
+        overlayDiv.innerHTML = `<div id="font-result">close</div>
+        <div class="container-font f_hide">
             <input type="number" id="fontcontrol" id="fontcontrol" min="0" max="100">
             <button id="copyfont">Copy</button>
         </div>`;
@@ -88,6 +111,8 @@ if(hasSetfont){
             let fontSize = window.getComputedStyle(currentelement).fontSize;
             let currentFont = fontSize.replace("px", "");
             document.querySelector('#fontcontrol').value = currentFont;
+
+            document.querySelector('.container-font').classList.remove('f_hide');
             });
         }
 
@@ -100,9 +125,13 @@ if(hasSetfont){
         function getAllFonts(){
             let fontfinal = "";
             for(let i = 0; i < elFont.length; i++){
-                fontfinal += "("+elFont[i].innerText.substring(0, 20) +' -> '+ window.getComputedStyle(elFont[i]).fontSize +') ---- ';
+                fontfinal += "> "+elFont[i].innerText.substring(0, 20) +' -> '+ window.getComputedStyle(elFont[i]).fontSize +' <br> ';
             }
-            document.querySelector('#font-result').innerHTML = "<div class='inner-result'>"+fontfinal+"</div>";
+            document.querySelector('#font-result').innerHTML = "<div class='inner-result'>"+fontfinal+"</div><button id='f_close'>X</button>";
+
+            document.querySelector('#f_close').addEventListener("click", function(){
+                document.querySelector('#font-result').innerHTML = "";
+            })
         }
 
         document.querySelector('#copyfont').addEventListener("click", function(){
