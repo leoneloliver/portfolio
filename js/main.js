258182b1e89171fb89faaf4ccd28f01eaead6472
Leonel Oliveira <leonel.oliveira@agilebits.com>
2025-01-09 21:47:16 -0500

    add skill list

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-/** 
+/**
  * ===================================================================
  * main js
  *
- * ------------------------------------------------------------------- 
- */ 
+ * -------------------------------------------------------------------
+ */
 
 (function($) {
 
@@ -11,16 +11,16 @@
 
 	/*---------------------------------------------------- */
 	/* Preloader
-	------------------------------------------------------ */ 
+	------------------------------------------------------ */
    $(window).load(function() {
 
-      // will first fade out the loading animation 
+      // will first fade out the loading animation
     	$("#loader").fadeOut("slow", function(){
 
         // will fade out the whole DIV that covers the website.
         $("#preloader").delay(100).fadeOut("slow");
 
-      });       
+      });
 
   	})
 
@@ -37,13 +37,13 @@
 
 	/*---------------------------------------------------- */
 	/* FitVids
-	------------------------------------------------------ */ 
+	------------------------------------------------------ */
   	$(".fluid-video-wrapper").fitVids();
 
 
 	/*---------------------------------------------------- */
 	/* Owl Carousel
-	------------------------------------------------------ */ 
+	------------------------------------------------------ */
 	$("#owl-slider").owlCarousel({
         navigation: false,
         pagination: true,
@@ -61,7 +61,7 @@
   	------------------------------------------------------- */
 	$('.alert-box').on('click', '.close', function() {
 	  $(this).parent().fadeOut(500);
-	});	
+	});
 
 
 	/*----------------------------------------------------- */
@@ -74,7 +74,7 @@
 
    	handler: function(direction) {
 
-      	if (direction === "down") {       		
+      	if (direction === "down") {
 
 			   stats.each(function () {
 				   var $this = $(this);
@@ -88,16 +88,16 @@
 				  	});
 				});
 
-       	} 
+       	}
 
        	// trigger once only
-       	this.destroy();      	
+       	this.destroy();
 
 		},
-			
+
 		offset: "90%"
-	
-	});	
+
+	});
 
 
 	/*---------------------------------------------------- */
@@ -107,9 +107,9 @@
 
 	containerProjects.imagesLoaded( function() {
 
-		containerProjects.masonry( {		  
+		containerProjects.masonry( {
 		  	itemSelector: '.folio-item',
-		  	resize: true 
+		  	resize: true
 		});
 
 	});
@@ -133,10 +133,10 @@
    // 	$.magnificPopup.close();
    // });
 
-	
+
 	/*-----------------------------------------------------*/
   	/* Navigation Menu
-   ------------------------------------------------------ */  
+   ------------------------------------------------------ */
    var toggleButton = $('.menu-toggle'),
        nav = $('.main-navigation');
 
@@ -150,13 +150,13 @@
 	});
 
    // nav items
-  	nav.find('li a').on("click", function() {   
+  	nav.find('li a').on("click", function() {
 
-   	// update the toggle button 		
-   	toggleButton.toggleClass('is-clicked'); 
+   	// update the toggle button
+   	toggleButton.toggleClass('is-clicked');
    	// fadeout the navigation panel
-   	nav.fadeOut();   		
-   	     
+   	nav.fadeOut();
+
   	});
 
 
@@ -164,7 +164,7 @@
   	/* Highlight the current section in the navigation bar
   	------------------------------------------------------ */
 	var sections = $("section"),
-	navigation_links = $("#main-nav-wrap li a");	
+	navigation_links = $("#main-nav-wrap li a");
 
 	sections.waypoint( {
 
@@ -176,12 +176,12 @@
 
 			if (direction === "up") active_section = active_section.prev();
 
-			var active_link = $('#main-nav-wrap a[href="#' + active_section.attr("id") + '"]');			
+			var active_link = $('#main-nav-wrap a[href="#' + active_section.attr("id") + '"]');
 
          navigation_links.parent().removeClass("current");
 			active_link.parent().addClass("current");
 
-		}, 
+		},
 
 		offset: '25%'
 	});
@@ -191,7 +191,7 @@
   	/* Smooth Scrolling
   	------------------------------------------------------ */
   	$('.smoothscroll').on('click', function (e) {
-	 	
+
 	 	e.preventDefault();
 
    	var target = this.hash,
@@ -203,13 +203,13 @@
       	window.location.hash = target;
       });
 
-  	});  
-  
+  	});
+
 
    /*---------------------------------------------------- */
 	/*  Placeholder Plugin Settings
-	------------------------------------------------------ */ 
-	$('input, textarea, select').placeholder()  
+	------------------------------------------------------ */
+	$('input, textarea, select').placeholder()
 
 
   	/*---------------------------------------------------- */
@@ -224,28 +224,28 @@
 
 			var sLoader = $('#submit-loader');
 
-			$.ajax({      	
+			$.ajax({
 
 		      type: "POST",
 		      url: "inc/sendEmail.php",
 		      data: $(form).serialize(),
-		      beforeSend: function() { 
+		      beforeSend: function() {
 
-		      	sLoader.fadeIn(); 
+		      	sLoader.fadeIn();
 
 		      },
 		      success: function(msg) {
 
 	            // Message was sent
 	            if (msg == 'OK') {
-	            	sLoader.fadeOut(); 
+	            	sLoader.fadeOut();
 	               $('#message-warning').hide();
 	               $('#contactForm').fadeOut();
-	               $('#message-success').fadeIn();   
+	               $('#message-success').fadeIn();
 	            }
 	            // There was an error
 	            else {
-	            	sLoader.fadeOut(); 
+	            	sLoader.fadeOut();
 	               $('#message-warning').html(msg);
 		            $('#message-warning').fadeIn();
 	            }
@@ -253,13 +253,13 @@
 		      },
 		      error: function() {
 
-		      	sLoader.fadeOut(); 
+		      	sLoader.fadeOut();
 		      	$('#message-warning').html("Something went wrong. Please try again.");
 		         $('#message-warning').fadeIn();
 
 		      }
 
-	      });     		
+	      });
   		}
 
 	});
@@ -267,7 +267,7 @@
 
  	/*----------------------------------------------------- */
   	/* Back to top
-   ------------------------------------------------------- */ 
+   ------------------------------------------------------- */
 	var pxShow = 300; // height on which the button will show
 	var fadeInTime = 400; // how slow/fast you want the button to show
 	var fadeOutTime = 400; // how slow/fast you want the button to hide
@@ -284,14 +284,14 @@
 				jQuery("#go-top").fadeOut(fadeOutTime);
 			}
 
-		}		
+		}
 
-	});		
+	});
 
 })(jQuery);
 
 
-// traking google events 
+// traking google events
 var trackClickEvent = function(event,category,label) {
 	gtag('event', event, {
 		'event_category': category,
@@ -323,21 +323,82 @@ const listSkills = [
 	{ icon: 'devicon-mysql-plain', text: 'mysql' },
 	{ icon: 'devicon-rails-plain-wordmark', text: 'ruby on rails' },
 	{ icon: 'devicon-firebase-plain-wordmark', text: 'firebase' },
-	
+	{ icon: 'devicon-html5-plain', text: 'html5' },
+	{ icon: 'devicon-css3-plain', text: 'css3' },
+	{ icon: 'devicon-javascript-plain', text: 'javascript' },
+	{ icon: 'devicon-typescript-plain', text: 'typescript' },
+	{ icon: 'devicon-jquery-plain', text: 'jquery' },
+	{ icon: 'devicon-bootstrap-plain', text: 'bootstrap' },
+	{ icon: 'devicon-tailwindcss-plain', text: 'tailwindcss' },
+	{ icon: 'devicon-sass-plain', text: 'sass' },
+	{ icon: 'devicon-storybook-plain', text: 'storybook' },
+	{ icon: 'devicon-jamstack-original', text: 'jamstack' },
+	{ icon: 'devicon-react-plain', text: 'reactjs' },
+	{ icon: 'devicon-nextjs-original-wordmark', text: 'nextjs' },
+	{ icon: 'devicon-vuejs-plain', text: 'vuejs' },
+	{ icon: 'devicon-npm-original-wordmark', text: 'npm' },
+	{ icon: 'devicon-gulp-plain', text: 'gulp' },
+	{ icon: 'devicon-git-plain', text: 'git' },
+	{ icon: 'devicon-github-plain', text: 'github' },
+	{ icon: 'devicon-php-plain', text: 'php' },
+	{ icon: 'devicon-mysql-plain', text: 'mysql' },
+	{ icon: 'devicon-rails-plain-wordmark', text: 'ruby on rails' },
+	{ icon: 'devicon-firebase-plain-wordmark', text: 'firebase' },
+	{ icon: 'devicon-html5-plain', text: 'html5' },
+	{ icon: 'devicon-css3-plain', text: 'css3' },
+	{ icon: 'devicon-javascript-plain', text: 'javascript' },
+	{ icon: 'devicon-typescript-plain', text: 'typescript' },
+	{ icon: 'devicon-jquery-plain', text: 'jquery' },
+	{ icon: 'devicon-bootstrap-plain', text: 'bootstrap' },
+	{ icon: 'devicon-tailwindcss-plain', text: 'tailwindcss' },
+	{ icon: 'devicon-sass-plain', text: 'sass' },
+	{ icon: 'devicon-storybook-plain', text: 'storybook' },
+	{ icon: 'devicon-jamstack-original', text: 'jamstack' },
+	{ icon: 'devicon-react-plain', text: 'reactjs' },
+	{ icon: 'devicon-nextjs-original-wordmark', text: 'nextjs' },
+	{ icon: 'devicon-vuejs-plain', text: 'vuejs' },
+	{ icon: 'devicon-npm-original-wordmark', text: 'npm' },
+	{ icon: 'devicon-gulp-plain', text: 'gulp' },
+	{ icon: 'devicon-git-plain', text: 'git' },
+	{ icon: 'devicon-github-plain', text: 'github' },
+	{ icon: 'devicon-php-plain', text: 'php' },
+	{ icon: 'devicon-mysql-plain', text: 'mysql' },
+	{ icon: 'devicon-rails-plain-wordmark', text: 'ruby on rails' },
+	{ icon: 'devicon-firebase-plain-wordmark', text: 'firebase' },
+
 ]
 
 function showAlertWhenDivReachesTop() {
 	const myDiv = document.querySelector('.intro-social'); // Replace 'myDiv' with the ID of your target div
 	const avatar = document.querySelector('.intro-info img');
-  
+
 	const divRect = myDiv.getBoundingClientRect();
 	const isDivAtTop = divRect.top <= 0;
-  
+
 	if (isDivAtTop) {
 		avatar.classList.remove('zoom');
 	}
   }
-  
+
   // Attach event listener to the scroll event of the window
   window.addEventListener('scroll', showAlertWhenDivReachesTop);
-  
+
+
+
+//   marquee
+
+document.addEventListener('DOMContentLoaded', function() {
+	const marquee = document.getElementById('marquee');
+	const content = marquee.innerHTML;
+
+	// Calculate animation duration based on content width
+	function updateDuration() {
+		const contentWidth = marquee.offsetWidth / 2;
+		const speed = 30; // Lower number = faster
+		const duration = contentWidth / speed;
+		marquee.style.animationDuration = `${duration}s`;
+	}
+
+	updateDuration();
+	window.addEventListener('resize', updateDuration);
+});
